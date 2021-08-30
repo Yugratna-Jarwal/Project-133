c0b3f37842cff8581729276e545c9ea25c00e360
Yugratna-Jarwal <77618975+Yugratna-Jarwal@users.noreply.github.com>
2021-08-30 12:06:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+status="";
+all=[];
+function preload()
+{
+    img=loadImage('Almirah.jpg');
+}
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status:detecting object";
+}
+function modelLoaded()
+{
+    console.log("model is loaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results)
+{
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects=results;
+}
+function draw()
+{
+    image(img,0,0,640,420);
+    if(status!="")
+    {
+        for(i=0;i<objects.length;i++)
+        {
+            document.getElementById("status").innerHTML="status:objects detected";
+            fill("#ff000d");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label + ""+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            stroke("#ff000d");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
